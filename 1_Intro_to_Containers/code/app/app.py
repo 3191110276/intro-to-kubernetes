bbfedf533984ede26ceb383a7feb17fb384dfb02
Michael Maurer <mimaurer@cisco.com>
2019-06-12 23:48:17 +0200

    updated example

--- 1_Intro_to_Containers/code/app/app.py
+++ 1_Intro_to_Containers/code/app/app.py
@@ -5,9 +5,10 @@ import socket
 app = Flask(__name__)
 
 @app.route("/<path:filename>")
-def serve_static():
+def serve_static(filename):
     root_dir = os.path.dirname(os.getcwd())
     return send_from_directory(os.path.join(root_dir, 'static'), filename)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
