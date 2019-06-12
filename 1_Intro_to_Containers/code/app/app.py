3756d7dd3eca0ba4d5f9175ac40ffcc94636277a
Michael Maurer <mimaurer@cisco.com>
2019-06-12 13:51:57 +0200

    Updated code

---
+++ 1_Intro_to_Containers/code/app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, send_from_directory
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route("/<path:filename>")
+def serve_static():
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static'), filename)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
